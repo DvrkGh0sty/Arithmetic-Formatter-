26cc9c8c18884e475c1cb6a7e7959b33235bc926
DvrkGh0sty <mutsvairoethan@gmail.com>
2025-10-21 07:24:38 +0300

    fixed my split problems function and changed into list comprehension to mkae more compact

--- Code/main.py
+++ Code/main.py
@@ -1,12 +1,10 @@
 def arithmetic_arranger(problems, show_answers=False):
     return problems
 def split_problems(problems):
-    after_split = []
-    for x in problems:
-        after_split.append(x.split())
+    after_split = [x.split()for x in problems]
     return after_split
+
 def main():
     print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])}')
 
 
-print(split_problems(['3 + 4', '67 + 41']))
